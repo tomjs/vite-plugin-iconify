9ce8ad6f659a06eb144cb158b7c20fef328d3f38
Tom Gao <tom@tomgao.cc>
2023-11-29 21:14:18 +0800

    revert: use fs-extra

--- src/cli/util.ts
+++ src/cli/util.ts
@@ -1,10 +1,10 @@
-import fs from 'node:fs';
 import path from 'node:path';
 import { cwd } from 'node:process';
-import { mkdirpSync, readJsonSync, ROOT } from '../utils';
+import fs from 'fs-extra';
+import { ROOT } from '../utils';
 import Logger from './logger';
 
-const { version } = readJsonSync(path.join(ROOT, 'package.json'));
+const { version } = fs.readJsonSync(path.join(ROOT, 'package.json'));
 
 export const NAME = 'ti';
 
@@ -53,7 +53,7 @@ export function createIconDataFile(sets: string[], options: IconifyDataOptions) 
 
   const outDir = path.join(cwd(), opts.path);
   if (!fs.existsSync(outDir)) {
-    mkdirpSync(outDir);
+    fs.mkdirpSync(outDir);
   }
 
   const list: any[] = [];
@@ -64,7 +64,7 @@ export function createIconDataFile(sets: string[], options: IconifyDataOptions) 
       return;
     }
 
-    const { prefix, info, icons } = readJsonSync(jsonFile);
+    const { prefix, info, icons } = fs.readJsonSync(jsonFile);
 
     list.push({
       prefix,
--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,3 @@
-import fs from 'node:fs';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 
@@ -15,32 +14,3 @@ export function urlConcat(...urls: string[]) {
 }
 
 export const ROOT = path.join(fileURLToPath(import.meta.url), '../..');
-
-/**
- * Synchronously creates a directory. like `mkdir -p`
- * @param path dir path
- */
-export function mkdirpSync(path: string) {
-  if (fs.existsSync(path)) {
-    return;
-  }
-
-  return fs.mkdirSync(path, { recursive: true });
-}
-
-/**
- * Synchronously removes files and directories
- */
-export function rmSync(path: string) {
-  if (fs.existsSync(path)) {
-    fs.rmSync(path, { recursive: true, force: true });
-  }
-}
-
-/**
- * read json file
- * @param path read file path
- */
-export function readJsonSync(path: string) {
-  return JSON.parse(fs.readFileSync(path, 'utf-8'));
-}
